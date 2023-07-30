f7459c7c9febe5d0cf30990d3b01fd1ee3ca8af6
MuazzezA <ayd.muazzez@gmail.com>
2023-07-30 16:27:39 +0300

    configure firebase

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import AppStack from './src/app/navigation/app-stack';
 import {SafeAreaView} from 'react-native';
 
+import firebase from '@react-native-firebase/app';
+import {firebaseConfig} from './src/app/config/firebase';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#abbbaa'}}>
