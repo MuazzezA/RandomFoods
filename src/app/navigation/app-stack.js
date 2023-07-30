96e70cfbb13e9a032ad8d4ea76e06b0e1df9ca7c
MuazzezA <ayd.muazzez@gmail.com>
2023-07-30 13:54:45 +0300

    create: navigation

---
+++ src/app/navigation/app-stack.js
@@ -0,0 +1,52 @@
+import React, {useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {HomeScreen, OnBoardingScreen, SplashScreen} from '../screens';
+
+const Stack = createNativeStackNavigator();
+
+const isOnBoarding = async () => {
+  try {
+    const onBoarding = await AsyncStorage.getItem('@completeOnBoarding');
+    return onBoarding === 'true' ? true : false;
+  } catch (error) {
+    return false;
+  }
+};
+
+const AppStack = () => {
+  useEffect(() => {
+    isOnBoarding().then(res => {
+      console.log(res);
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        {isOnBoarding ? (
+          <>
+            <Stack.Screen name="splash">
+              {props => <SplashScreen {...props} onboarding={isOnBoarding} />}
+            </Stack.Screen>
+            <Stack.Screen name="home" component={HomeScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="splash">
+              {props => <SplashScreen {...props} onboarding={isOnBoarding} />}
+            </Stack.Screen>
+            <Stack.Screen name="onbording" component={OnBoardingScreen} />
+            <Stack.Screen name="home" component={HomeScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppStack;
