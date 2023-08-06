b098a8f48b58795644c3931eb48367a97ed17eff
MuazzezA <ayd.muazzez@gmail.com>
2023-08-06 19:40:11 +0300

    Add : filter to home screen

--- src/app/screens/home-screen/index.js
+++ src/app/screens/home-screen/index.js
@@ -1,12 +1,16 @@
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import {firebase} from '@react-native-firebase/database';
 import {firebaseConfig} from '../../config/firebase';
+import {Header, Button, LoadingData} from '../../components';
 import styles from './styles';
 
 export const HomeScreen = () => {
   const [data, setData] = React.useState('');
   const [categoryKeys, setCategoryKeys] = React.useState([]);
+  const [error, setError] = React.useState('');
+  const [selectedCategory, setSelectedCategory] = React.useState(null);
+  const [selectedFood, setSelectedFood] = React.useState(null);
 
   const getFirebaseData = () => {
     const reference = firebase
@@ -20,14 +24,80 @@ export const HomeScreen = () => {
     });
   };
 
+  const getRandomCategory = () => {
+    const randomIndex = Math.floor(Math.random() * categoryKeys.length);
+    setSelectedCategory(categoryKeys[randomIndex]);
+  };
+
+  const getRandomFood = (isAll = false) => {
+    if (isAll) {
+      getRandomCategory();
+    }
+
+    if (selectedCategory) {
+      const specialFood = data[selectedCategory];
+      const randomIndex = Math.floor(Math.random() * specialFood.data.length);
+
+      setError('');
+      setSelectedFood({
+        food: specialFood.data[randomIndex],
+        type: specialFood.name,
+      });
+    } else {
+      setError('Lütfen önce kategori seçiniz.');
+    }
+  };
+
   useEffect(() => {
     getFirebaseData();
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>{JSON.stringify(data[categoryKeys[0]])}</Text>
-      <Text>{JSON.stringify(categoryKeys)}</Text>
+    <View style={styles.root}>
+      <Header title={'Anasayfa'} />
+
+      {data ? (
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          showsVerticalScrollIndicator={false}>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>
+              Seçilen Kategori :{' '}
+              <Text style={styles.foodText}>
+                {data[selectedCategory]?.name}
+              </Text>
+            </Text>
+            <Text style={styles.text}>
+              Yemeğiniz :{' '}
+              <Text style={styles.foodText}>{selectedFood?.food}</Text>
+            </Text>
+            <Text style={styles.error}>{error}</Text>
+          </View>
+          <View style={styles.listContainer}>
+            {categoryKeys.map((item, index) => {
+              return (
+                <Button
+                  key={index}
+                  preset={'secondary'}
+                  title={data[categoryKeys[index]].name}
+                  onPress={() => setSelectedCategory(categoryKeys[index])}
+                />
+              );
+            })}
+          </View>
+          <Button
+            title="Rastgele Kategori"
+            onPress={() => getRandomCategory()}
+          />
+          <Button
+            title="Tamamen Rastgele"
+            onPress={() => getRandomFood({isAll: true})}
+          />
+          <Button title="Üret" onPress={() => getRandomFood()} />
+        </ScrollView>
+      ) : (
+        <LoadingData />
+      )}
     </View>
   );
 };
--- src/app/screens/home-screen/styles.js
+++ src/app/screens/home-screen/styles.js
@@ -1,15 +1,58 @@
 import {StyleSheet} from 'react-native';
+import {colors, sizes} from '../../utils/theme';
 
 export default StyleSheet.create({
+  root: {
+    flex: 1,
+
+    alignItems: 'center',
+  },
+  scrollContent: {
+    flexGrow: 1,
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
     justifyContent: 'center',
     alignItems: 'center',
   },
   foodText: {
-    fontSize: 20,
+    fontSize: sizes.medium,
     fontWeight: 'bold',
-    margin: 20,
+    color: colors.error,
+  },
+  listContainer: {
+    marginTop: sizes.padding,
+    marginBottom: sizes.padding * 5,
+    backgroundColor: colors.white,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: sizes.shadowOpacity,
+    shadowRadius: sizes.shadowRadius,
+    elevation: sizes.elevation,
+    borderRadius: sizes.radius,
+  },
+  textContainer: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginVertical: sizes.padding,
+  },
+  text: {
+    fontSize: sizes.medium,
+    fontWeight: 'bold',
+    color: colors.black,
+    lineHeight: sizes.large,
+    marginVertical: sizes.padding,
+  },
+  error: {
+    fontSize: sizes.small,
+    color: colors.error,
   },
 });
